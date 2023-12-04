d27cd7dd567b2da7d422f2b89b0520542e47bd8a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-04 22:02:24 +0000

    initial tests

--- test/nextcloud_snap_tests_test.dart
+++ test/nextcloud_snap_tests_test.dart
@@ -1,12 +1,47 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:math';
+import 'dart:typed_data';
 
-import 'package:nextcloud_snap_tests/nextcloud_snap_tests.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nextcloud/nextcloud.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('NextcloudSnapTests', () {
+    final random = Random();
+
+    final client = NextcloudClient(
+      Uri.parse('http://localhost'),
+      loginName: 'admin',
+      password: 'admin',
+    );
+
+    for (var i = 0; i < 20; i++) {
+      test('upload small files $i', () async {
+        final path = PathUri.parse('$i.bin');
+        final length = i * 1024;
+        final data = Uint8List.fromList(
+            List.generate(length, (final index) => random.nextInt(256)));
+
+        final putResponse = await client.webdav.put(
+          data,
+          path,
+        );
+        expect(putResponse.statusCode, 201);
+
+        final propfindResponse = await client.webdav.propfind(
+          path,
+          prop: WebDavPropWithoutValues.fromBools(
+            davgetcontentlength: true,
+            ocsize: true,
+          ),
+          depth: WebDavDepth.zero,
+        );
+        final props = propfindResponse.responses.single.propstats.single.prop;
+        expect(props.davgetcontentlength, length);
+        expect(props.ocsize, length);
+
+        final getResponse = await client.webdav.get(path);
+        expect(getResponse, data);
+      });
+    }
   });
 }
